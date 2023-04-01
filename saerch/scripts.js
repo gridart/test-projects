e99ef1f698ddfeb95d55d950bd8130b5a5944b18
gridart <111399942+gridart@users.noreply.github.com>
2023-04-01 23:39:36 +0200

    Add files via upload

---
+++ saerch/scripts.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  var input, filter, cards, cardContainer, title, i;
+  input = document.getElementById("myFilter");
+  filter = input.value.toUpperCase();
+  cardContainer = document.getElementById("myProducts");
+  cards = cardContainer.getElementsByClassName("card");
+  for (i = 0; i < cards.length; i++) {
+    title = cards[i].querySelector(".card-title");
+    if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+      cards[i].style.display = "";
+    } else {
+      cards[i].style.display = "none";
+    }
+  }
+}
